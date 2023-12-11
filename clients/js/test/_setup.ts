54896663874e8fc21f5a52d454620c237bcd3862
Loris Leiva <loris.leiva@gmail.com>
2023-12-11 11:15:58 +0000

    Update web3.js and refactor tests

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -1,17 +1,33 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@solana/webcrypto-ed25519-polyfill';
 
+import { pipe } from '@solana/functional';
 import {
+  generateKeyPairSigner,
+  signTransactionWithSigners,
+} from '@solana/signers';
+import {
+  Address,
+  CompilableTransaction,
+  ITransactionWithBlockhashLifetime,
+  createDefaultAirdropRequester,
   createDefaultRpcSubscriptionsTransport,
   createDefaultRpcTransport,
+  createDefaultTransactionSender,
   createSolanaRpc,
   createSolanaRpcSubscriptions,
+  createTransaction,
+  lamports,
+  setTransactionFeePayer,
+  setTransactionLifetimeUsingBlockhash,
 } from '@solana/web3.js';
 
-export const createClient = (): {
+type Client = {
   rpc: ReturnType<typeof createSolanaRpc>;
   rpcSubscriptions: ReturnType<typeof createSolanaRpcSubscriptions>;
-} => {
+};
+
+export const createClient = (): Client => {
   const rpc = createSolanaRpc({
     transport: createDefaultRpcTransport({ url: 'http://127.0.0.1:8899' }),
   });
@@ -23,3 +39,45 @@ export const createClient = (): {
 
   return { rpc, rpcSubscriptions };
 };
+
+export const generateKeyPairSignerWithSol = async (
+  client: Client,
+  putativeLamports: bigint = 1_000_000_000n
+) => {
+  const airdropRequester = createDefaultAirdropRequester(client);
+  const signer = await generateKeyPairSigner();
+  await airdropRequester({
+    recipientAddress: signer.address,
+    lamports: lamports(putativeLamports),
+    commitment: 'confirmed',
+  });
+  return signer;
+};
+
+export const createDefaultTransaction = async (
+  client: Client,
+  feePayer: Address
+) => {
+  const { value: latestBlockhash } = await client.rpc
+    .getLatestBlockhash()
+    .send();
+  return pipe(
+    createTransaction({ version: 0 }),
+    (tx) => setTransactionFeePayer(feePayer, tx),
+    (tx) => setTransactionLifetimeUsingBlockhash(latestBlockhash, tx)
+  );
+};
+
+export const signAndSendTransaction = async (
+  client: Client,
+  transaction: CompilableTransaction & ITransactionWithBlockhashLifetime
+) => {
+  const signedTransaction = await signTransactionWithSigners(transaction);
+  await createDefaultTransactionSender(client)(signedTransaction, {
+    commitment: 'confirmed',
+  });
+};
+
+export const getBalance = async (client: Client, address: Address) =>
+  (await client.rpc.getBalance(address, { commitment: 'confirmed' }).send())
+    .value;
--- clients/js/test/transferSol.test.ts
+++ clients/js/test/transferSol.test.ts
@@ -1,85 +1,38 @@
 import { pipe } from '@solana/functional';
 import { lamports } from '@solana/rpc-types';
-import {
-  generateKeyPairSigner,
-  signTransactionWithSigners,
-} from '@solana/signers';
-import {
-  appendTransactionInstruction,
-  createDefaultAirdropRequester,
-  createDefaultTransactionSender,
-  createTransaction,
-  setTransactionFeePayer,
-  setTransactionLifetimeUsingBlockhash,
-} from '@solana/web3.js';
+import { generateKeyPairSigner } from '@solana/signers';
+import { appendTransactionInstruction } from '@solana/web3.js';
 import test from 'ava';
+import { getTransferSolInstruction } from '../src';
 import {
-  getAddMemoInstruction,
-  getSetComputeUnitLimitInstruction,
-  getTransferSolInstruction,
-} from '../src';
-import { createClient } from './_setup';
+  createClient,
+  createDefaultTransaction,
+  generateKeyPairSignerWithSol,
+  getBalance,
+  signAndSendTransaction,
+} from './_setup';
 
 test('it can transfer SOL from one account to another', async (t) => {
-  // Given a context object.
+  // Given a source account with 3 SOL and a destination account with no SOL.
   const client = createClient();
-  const airdropRequester = createDefaultAirdropRequester(client);
-
-  // And a source account with 3 SOL.
-  const source = await generateKeyPairSigner();
-  await airdropRequester({
-    recipientAddress: source.address,
-    lamports: lamports(3_000_000_000n),
-    commitment: 'confirmed',
-  });
-
-  // And a destination account with no SOL.
+  const source = await generateKeyPairSignerWithSol(client, 3_000_000_000n);
   const destination = (await generateKeyPairSigner()).address;
 
   // When the source account transfers 1 SOL to the destination account.
-  const { value: latestBlockhash } = await client.rpc
-    .getLatestBlockhash()
-    .send();
-  const setComputeUnitLimit = getSetComputeUnitLimitInstruction({
-    units: 600_000,
-  });
   const transferSol = getTransferSolInstruction({
     source,
     destination,
     amount: 1_000_000_000,
   });
-  const addMemo = getAddMemoInstruction({ memo: "I'm transferring some SOL!" });
-
-  const transaction = pipe(
-    createTransaction({ version: 0 }),
-    (tx) => setTransactionFeePayer(source.address, tx),
-    (tx) => setTransactionLifetimeUsingBlockhash(latestBlockhash, tx),
-    (tx) => appendTransactionInstruction(setComputeUnitLimit, tx),
+  await pipe(
+    await createDefaultTransaction(client, source.address),
     (tx) => appendTransactionInstruction(transferSol, tx),
-    (tx) => appendTransactionInstruction(addMemo, tx)
+    (tx) => signAndSendTransaction(client, tx)
   );
-
-  const signedTransaction = await signTransactionWithSigners(transaction);
-  await createDefaultTransactionSender(client)(signedTransaction, {
-    commitment: 'confirmed',
-  });
 
   // Then the source account new has less than 2 SOL.
-  t.true(
-    (
-      await client.rpc
-        .getBalance(source.address, { commitment: 'confirmed' })
-        .send()
-    ).value < 2_000_000_000n
-  );
+  t.true((await getBalance(client, source.address)) < 2_000_000_000n);
 
   // And the destination account has exactly 1 SOL.
-  t.is(
-    (
-      await client.rpc
-        .getBalance(destination, { commitment: 'confirmed' })
-        .send()
-    ).value,
-    lamports(1_000_000_000n)
-  );
+  t.is(await getBalance(client, destination), lamports(1_000_000_000n));
 });
